2c1802bd4bda88ae2e9aa88a36afa9a2981885d0
Henrik Austad <henrik@austad.us>
2010-02-15 10:18:46 +0100

    MetaDoc Client: Add GPL v3 licensing to the files
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- Client/MetaHTTP.py
+++ Client/MetaHTTP.py
@@ -1,12 +1,30 @@
-"""
-XML_Client. Simple client for pushing XML over HTTPS
-"""
+# -*- coding: utf-8 -*-
+#
+#            MetaHTTP.py is part of MetaDoc (Client).
+#
+# All of MetaDoc is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# MetaDoc is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MetaDoc.  If not, see <http://www.gnu.org/licenses/>.
+# The API interface
+#
 #!/usr/bin/env python
 # The HTTPSClientAuthHandler is inspired by
 #       http://www.threepillarsoftware.com/soap_client_auth
 #
 import urllib, urllib2, httplib
 
+"""
+XML_Client. Simple client for pushing XML over HTTPS
+"""
 class HTTPSClientAuthHandler(urllib2.HTTPSHandler):
     """
     HTTPSClientAuthHandler extends HTTPSHandler
