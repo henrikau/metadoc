62fd3df172f9b62995e4e78f39a027f39f4fcb43
Bjørnar Grip Fjær <bjornar.fjar@gmail.com>
2010-06-16 15:16:26 +0200

    Changed to using lxml. Added a check response that parses the response from the server and checks if any of the entries have been mutilated.

--- client/metadoc.py
+++ client/metadoc.py
@@ -15,7 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with MetaDoc.  If not, see <http://www.gnu.org/licenses/>.
 # The API interface
-import xml.etree.ElementTree
+
+import logging
+import lxml.etree
 
 class MetaDoc:
     """
@@ -32,9 +34,9 @@ class MetaDoc:
 
     def _create_root(self):
         self.root = None
-        self.root = xml.etree.ElementTree.Element("MetaDoc",
-                                                  version="1.0",
-                                                  fullUpdate=self.fullUpdate)
+        self.root = lxml.etree.Element("MetaDoc",
+                                        version="1.0",
+                                        fullUpdate=self.fullUpdate)
 
     def reg_meta_element(self, me):
         """
@@ -57,6 +59,48 @@ class MetaDoc:
             self.root.append(me.get_xml_element())
 
 
-        return xml.etree.ElementTree.tostring(self.root, "UTF-8")
+        return lxml.etree.tostring(self.root, encoding=unicode)
 
+    def find_id(self, locate_id):
+        """ Attempts to locate the element with a given ID. """
+        for me in self.metaelements:
+            element = me.find_id(locate_id)
+            if element:
+                return element
+        return None
 
+    def check_response(self, xml_response):
+        """ Function that checks the response from server.
+        xml_response should be the XML response from the server when self was 
+        sent.
+
+        """
+        try:
+            response = lxml.etree.fromstring(xml_response)
+        except lxml.etree.XMLSyntaxError, e:
+            logging.error("Error parsing server response: ", e)
+        else:
+            receipt = response.find("receipt")
+            r_entries = receipt.findall("r_entry")
+            for r_entry in r_entries:
+                element = self.find_id(r_entry.attrib.get("id"))
+                if not r_entry.attrib.get("code") == '1000':
+                    if element:
+                        err_str = "Error %s on \"%s\" element.\nElement attributes: %s" % (
+                                        r_entry.attrib.get("code"),
+                                        element.xml_tag_name,
+                                        element.attributes
+                                        )
+                    else:
+                        err_str = "Error %s on element. " % r_entry.attrib.get("code")
+                    if r_entry.attrib.get("note"):
+                        err_str = "%s\nError note: %s" % (err_str, r_entry.attrib.get("note"))
+                    if r_entry.text:
+                        err_str = "%s\nError message:\n%s" % (err_str, r_entry.text)
+                    logging.error(err_str)
+                else:
+                    if element:
+                        info_str = "Element \"%s\" successfully added. (Attributes: %s)" % (element.xml_tag_name, element.attributes)
+                    else:
+                        info_str = "Element successfully added."
+                    logging.info(info_str)
